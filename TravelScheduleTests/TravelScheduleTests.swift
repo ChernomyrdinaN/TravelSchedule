824ec0027515a4a8a9103e55128c96780891da46
Natalya <chernomyrdina17@gmail.com>
2025-08-21 16:37:38 +0300

    Initial Commit

---
+++ TravelScheduleTests/TravelScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleTests.swift
+//  TravelScheduleTests
+//
+//  Created by Наталья Черномырдина on 21.08.2025.
+//
+
+import Testing
+@testable import TravelSchedule
+
+struct TravelScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
