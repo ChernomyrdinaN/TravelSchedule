2e396d5de8ec12bc9aefa30c9fa40c472c008182
Natalya <chernomyrdina17@gmail.com>
2025-08-22 12:43:18 +0300

    Первоначальный коммит. Настройка проекта с использованием SwiftUI и структуры MVVM.

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelSchedulerApp.swift
+//  TravelSchedule
+//
+//  Created by Наталья Черномырдина on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelSchedule/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TravelSchedule
+//
+//  Created by Наталья Черномырдина on 21.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
