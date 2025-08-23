d4325d77fb74dcba333a98dc437285e746399ee0
Natalya <chernomyrdina17@gmail.com>
2025-08-23 19:47:11 +0300

    Вынесен API ключ в константу, протестировна генерация и работа сервиса NearestStationsService

---
+++ TravelSchedule/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TravelSchedule
+//
+//  Created by Наталья Черномырдина on 23.08.2025.
+//
+
+import Foundation
+
+enum Constants {
+  
+    static let yandexAPIKey = "17dbdf13-ff18-4549-bbc7-ca3b8cd8b04b"
+    static let yandexAPIBaseURL = "https://api.rasp.yandex.net/v3.0"
+}
