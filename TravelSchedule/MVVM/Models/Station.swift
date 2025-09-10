672ae624610f188908293982ae50579c10b562fd
Natalya <chernomyrdina17@gmail.com>
2025-09-11 01:34:10 +0300

    Сверстан экран со списком станций  в указанном городе (с возможностью поиска)

--- TravelSchedule/MVVM/Models/Station.swift
+++ TravelSchedule/MVVM/Models/Station.swift
@@ -11,6 +11,7 @@ struct Station: Identifiable, Hashable {
     let id = UUID()
     let name: String
     
+    // MARK: - Mock Data
     static let mockData = [
         Station(name: "Москва"),
         Station(name: "Санкт-Петербург"),
@@ -23,4 +24,32 @@ struct Station: Identifiable, Hashable {
         Station(name: "Нижний Новгород"),
         Station(name: "Новосибирск")
     ]
+    
+    static func mockStations(for city: String) -> [Station] {
+        switch city {
+        case "Москва":
+            return [
+                Station(name: "Киевский вокзал"),
+                Station(name: "Курский вокзал"),
+                Station(name: "Ярославский вокзал"),
+                Station(name: "Белорусский вокзал"),
+                Station(name: "Савеловский вокзал"),
+                Station(name: "Ленинградский вокзал")
+            ]
+        case "Санкт-Петербург":
+            return [
+                Station(name: "Балтийский вокзал"),
+                Station(name: "Витебский вокзал"),
+                Station(name: "Ладожский вокзал"),
+                Station(name: "Московский вокзал"),
+                Station(name: "Финляндский вокзал")
+            ]
+        default:
+            return [
+                Station(name: "\(city) Центральный"),
+                Station(name: "\(city) Северный"),
+                Station(name: "\(city) Южный")
+            ]
+        }
+    }
 }
